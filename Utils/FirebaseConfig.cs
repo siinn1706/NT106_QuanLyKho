5adb05e520801cdc9642f246d6dcee32f9b5d6e1
trihoang0901 <trihoang0901@gmail.com>
2025-10-28 21:17:23 +0700

    api firebase

---
+++ Utils/FirebaseConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT106_Nhom12_Pro.Utils
+{
+    public static class FirebaseConfig
+    {
+        public const string apiKey = "AIzaSyCCq_2V9JxjA5GHoXiIpORUBR83GNR2UBk";
+        public const string authDomain = "nt106-quanlykho.firebaseapp.com";
+        public const string projectId = "nt106-quanlykho";
+        public const string storageBucket = "nt106-quanlykho.firebasestorage.app";
+        public const string messagingSenderId = "1044513655423";
+        public const string appId = "1:1044513655423:web:54742c381fa1044d7dcd4a";
+        public const string measurementId = "G-3DQBF55DYM";
+    }
+}
