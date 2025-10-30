7388a13c36fa621ef4cc6f5280f69a6040e0ba45
trihoang0901 <trihoang0901@gmail.com>
2025-10-30 13:51:37 +0700

    Clear Firebase configuration constants
    Removed Firebase api

--- Utils/FirebaseConfig.cs
+++ Utils/FirebaseConfig.cs
@@ -9,12 +9,7 @@ namespace NT106_Nhom12_Pro.Utils
 {
     public static class FirebaseConfig
     {
-        public const string apiKey = "AIzaSyCCq_2V9JxjA5GHoXiIpORUBR83GNR2UBk";
-        public const string authDomain = "nt106-quanlykho.firebaseapp.com";
-        public const string projectId = "nt106-quanlykho";
-        public const string storageBucket = "nt106-quanlykho.firebasestorage.app";
-        public const string messagingSenderId = "1044513655423";
-        public const string appId = "1:1044513655423:web:54742c381fa1044d7dcd4a";
-        public const string measurementId = "G-3DQBF55DYM";
+        public const string apiKey = "";
+        public const string authDomain = "";
     }
 }
